408dd47406dc5cfe994987b2872f8b696fc5b66c
carlosmartos13 <carlosmartos04@gmail.com>
2019-08-19 11:26:23 -0300

    Aula 20 - crado formulario HTMl para editar usuario

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -14,6 +14,9 @@ class UserController {
             btn.disabled = true;
             
           let values  = this.getValues();
+
+          if (!values) return false;
+
           this.getPhoto().then(
               (content) => {
 
@@ -123,6 +126,8 @@ class UserController {
       
             let tr = document.createElement('tr');
 
+            tr.dataset.user = JSON.stringify(dataUser);
+
             tr.innerHTML = `
           
                 <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
@@ -137,8 +142,28 @@ class UserController {
               
                     `;
                     this.tableEl.appendChild(tr);
+                    this.updateCount();
+
+                
           
     }
+    // contador de usuarios
+    updateCount(){
+        let numberUsers = 0;
+        let numberAdmin = 0;
+        [...this.tableEl.children].forEach( tr => {
+
+            numberUsers++;
+            
+            let user = JSON.parse(tr.dataset.user);
+
+            if (user._admin) numberAdmin++; 
+
+        });
+        
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
+    }// Fim do Contador de usuarios
 
 
 }
